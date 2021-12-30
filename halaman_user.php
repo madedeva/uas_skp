12506240e68311e57fea249bfb89fe20d23f61a9
madedeva <deva.kerti@undiksha.ac.id>
2021-12-30 13:18:11 +0800

    update 8.0

--- halaman_user.php
+++ halaman_user.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Mahasiswa</title>
+	<title>e-SKP FTK | Mahasiswa</title>
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
